49f20720168259e9759c38c97c0d1e5ba99c4046
moikheck <whatthemheckman@gmail.com>
2021-10-26 17:03:56 -0400

    Added Shortsword animation

--- project/src/Shortsword.gd
+++ project/src/Shortsword.gd
@@ -1,22 +1,17 @@
 extends Area2D
 
-
-const BRAND := "shortsword"
+const BRAND = "Shortsword"
 const NORMAL_PATH := "res://Assets/Swords/Shortsword.png"
 const IRON_PATH := "res://Assets/Swords/Iron Shortsword.png"
 const GOLD_PATH := "res://Assets/Swords/Golden Shortsword.png"
 
 
-var type := "normal"
+var type := "Normal"
 
-func _ready():
-	#sword.construct(type, BRAND)
-	pass
-	
+
 func updateType(new_type):
 	type = new_type
-	if type == "golden":
+	if type == "Golden":
 		$Sprite.texture = load(GOLD_PATH)
-	elif type == "iron":
+	elif type == "Iron":
 		$Sprite.texture = load(IRON_PATH)
-	#sword.construct(type, BRAND)
