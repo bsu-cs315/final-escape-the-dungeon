a16fa04e0894da25e9d00db1d868d0c031151786
moikheck <whatthemheckman@gmail.com>
2021-11-06 11:54:29 -0400

    Fixed formatting

--- project/src/Level.gd
+++ project/src/Level.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
+
 func _process(_delta):
-	var noEnemies = get_tree().get_nodes_in_group("Enemies").empty()
-	if noEnemies:
+	var no_enemies = get_tree().get_nodes_in_group("Enemies").empty()
+	if no_enemies:
 		$Player.active = false
 		$Player/HUD/EndMessage.text = "You Win"
 		$Player/HUD/RestartButton.visible = true
