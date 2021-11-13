b75ac42455353406c0a06d3589f362055dc99dd3
moikheck <whatthemheckman@gmail.com>
2021-11-12 22:07:38 -0500

    Weapons/Arrow now fully rotates with mouse position

--- project/src/Player.gd
+++ project/src/Player.gd
@@ -1,10 +1,7 @@
 extends KinematicBody2D
 
 
-const LEFT_ROTATION := -90
-const RIGHT_ROTATION := 90
-const UP_ROTATION := 0
-const DOWN_ROTATION := 180
+const ROTATION_OFFSET := 90
 const CENTER_POINT := Vector2(468,300)
 const WALL_COLLISION := Vector2(0,0)
 const DIRECTION_MULT := 1.5
@@ -24,6 +21,7 @@ var current_weapon
 var is_active := true
 var is_paused := false
 var facing := "left"
+var angle_facing := 0.0000
 
 
 var _is_hurt := false
@@ -73,18 +71,6 @@ func _physics_process(_delta):
 			direction.x -= 1
 		if Input.is_action_pressed("move_right"):
 			direction.x += 1
-			
-		var mouse_pos = get_viewport().get_mouse_position()
-		var x = mouse_pos.x - CENTER_POINT.x
-		var y = mouse_pos.y - CENTER_POINT.y
-		if x < 0 and abs(x) > abs(y * DIRECTION_MULT):
-			facing = "left"
-		elif x >= 0 and abs(x) > abs(y * DIRECTION_MULT):
-			facing = "right"
-		elif y < 0 and abs(y * DIRECTION_MULT) > abs(x):
-			facing = "up"
-		else:
-			facing = "down"
 			
 		direction = direction.normalized()
 		
@@ -136,22 +122,9 @@ func attack():
 
 
 func position_weapon():
-	if facing == "up":
-		current_weapon.position = $AttackUp.position
-		current_weapon.position.y -= current_weapon.position_extension
-		current_weapon.rotation_degrees += UP_ROTATION
-	elif facing == "down":
-		current_weapon.position = $AttackDown.position
-		current_weapon.position.y += current_weapon.position_extension
-		current_weapon.rotation_degrees += DOWN_ROTATION
-	elif facing == "left":
-		current_weapon.position = $AttackLeft.position
-		current_weapon.position.x -= current_weapon.position_extension
-		current_weapon.rotation_degrees += LEFT_ROTATION
-	elif facing == "right":
-		current_weapon.position = $AttackRight.position
-		current_weapon.position.x += current_weapon.position_extension
-		current_weapon.rotation_degrees += RIGHT_ROTATION
+	angle_facing = rad2deg(get_angle_to(get_global_mouse_position())) + ROTATION_OFFSET
+	current_weapon.position = $AttackRight.position
+	current_weapon.rotation_degrees += angle_facing
 
 
 func remove_weapon():
@@ -163,7 +136,7 @@ func spawn_arrow(damage):
 	var arrow = load("res://src/Arrow.tscn").instance()
 	get_parent().call_deferred("add_child", arrow)
 	arrow.damage = damage
-	arrow.fire(position, facing)
+	arrow.fire(position, angle_facing)
 
 
 func killed():
