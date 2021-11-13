9f44842b1658ae9e488038e5d2ade999913d8755
moikheck <whatthemheckman@gmail.com>
2021-11-12 20:41:56 -0500

    Added keys and way to update key count to HUD

--- project/src/HUD.gd
+++ project/src/HUD.gd
@@ -1,6 +1,10 @@
 extends Control
 
 
+const KEY_OUTLINE := "res://assets/Items/key_outline.png"
+const KEY_TEXTURED := "res://assets/Items/key.png"
+
+
 func _on_RestartButton_pressed():
 	 var _ignored = get_tree().change_scene("res://src/TitleScreen.tscn")
 
@@ -9,3 +13,10 @@ func update_weapons(primary_weapon, secondary_weapon):
 	$SecondaryWindow/Weapon.texture = secondary_weapon.get_texture()
 	$PrimaryWindow/Weapon.texture = primary_weapon.get_texture()
 
+
+func update_key_count(value):
+	$KeyLabel.text = "x" + str(value)
+	if value == 0:
+		$KeyLogo.texture = load(KEY_OUTLINE)
+	else:
+		$KeyLogo.texture = load(KEY_TEXTURED)
