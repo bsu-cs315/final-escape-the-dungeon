adc6e9c36d9958dbbb458863f6537cd1b804ec48
Cameron <caslash@bsu.edu>
2021-11-30 15:11:27 -0500

    Added sliders for music, sound effects, and master volume

---
+++ project/Levels/Main/MusicSlider.gd
@@ -0,0 +1,11 @@
+extends HSlider
+
+export var audio_bus_name := "Music"
+
+onready var _bus := AudioServer.get_bus_index(audio_bus_name)
+
+func _ready():
+	value = db2linear(AudioServer.get_bus_volume_db(_bus))
+
+func _on_MusicSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(_bus, linear2db(value))
